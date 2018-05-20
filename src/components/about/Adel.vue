a232b3ab2098093c8a8219008a9b91a0b30d9441
526697296 <526697296@qq.com>
2018-05-20 23:56:42 +0800

    add adel.vue

---
+++ src/components/about/Adel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="adel">
+    adel
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Adel',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
