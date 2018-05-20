f1b6891c94e9b0747ec589309c371dbdd73b7920
526697296 <526697296@qq.com>
2018-05-20 23:31:35 +0800

    构建home页

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,39 @@
 <template>
-  <div id="home">
-    home
-    
+  <div class="home">
+    <div class="row">
+      <div class="homes text-center col-sm-12">
+        <div class="background">
+          <h1>欢迎来到ZHOUJH点餐系统。</h1>
+          <button @click="gotoMenu" class="btn btn-success">进入系统</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
-
 <script>
-
 export default {
   name: 'Home',
   data(){
     return{
-
     }
   },
-  components:{
-    
+  components:{ 
+  },
+  methods:{
+    gotoMenu(){
+    }
   }
 }
 </script>
-
 <style scoped>
-
+  .home{
+    margin-top: 6px;
+  }
+  .homes{
+    background: url('../../src/assets/bg.jpg') no-repeat;
+    height:85vh;
+  }
+  .background{
+    margin-top: 100px;
+  }
 </style>
