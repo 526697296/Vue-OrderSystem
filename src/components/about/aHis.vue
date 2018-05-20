a6dca87ef1ce793c957543cee78d7aaadf915771
526697296 <526697296@qq.com>
2018-05-21 00:10:38 +0800

    add ahis.vue

---
+++ src/components/about/aHis.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="ahis">
+    ahis
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Ahis',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
