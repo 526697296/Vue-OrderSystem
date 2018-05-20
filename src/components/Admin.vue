e49687efdcf8c7c8fed47c410f36413fa8848f6c
526697296 <526697296@qq.com>
2018-05-20 23:14:02 +0800

    add admin.vue

---
+++ src/components/Admin.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="admin">
+    admin
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Admin',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
