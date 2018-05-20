e5118756641a84837d2c4b64f21f2938792ef4b9
526697296 <526697296@qq.com>
2018-05-21 00:12:20 +0800

    add aogu.vue

---
+++ src/components/about/Aogu.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="aogu">
+    aogu
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Aogu',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
