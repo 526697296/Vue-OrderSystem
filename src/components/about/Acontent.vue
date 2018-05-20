bba3364344fe58d71bb1899eb8e0fdc39f5e3f5a
526697296 <526697296@qq.com>
2018-05-20 23:54:03 +0800

    add acontent.vue

---
+++ src/components/about/Acontent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="acontent">
+    acontent
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Acontent',
+  data(){
+    return{
+
+    }
+  },
+  components:{
+    
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
