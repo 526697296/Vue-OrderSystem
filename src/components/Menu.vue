3d6745afcbceae700d40038cbeee62084dbc949b
526697296 <526697296@qq.com>
2018-05-21 20:43:38 +0800

    构建Menu.vue页面

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,7 +1,26 @@
 <template>
   <div id="menu">
-    menu
-    
+    <div class="col-sm-12">
+      <table class="table">
+        <thead class="thead-default">
+          <tr>
+            <th>规格</th>
+            <th>价格</th>
+            <th>购买</th>
+          </tr>
+        </thead>
+        <tbody v-for="item in getMenuItems" :key="item.name">
+          <tr>
+            <td><strong>{{item.name}}</strong></td>
+          </tr>
+          <tr v-for="option in item.options" :key="option.size">
+            <td>{{option.size}}</td>
+            <td>{{option.price}}</td>
+            <td><button class="btn btn-sm btn-outline-success">+</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -11,7 +30,42 @@ export default {
   name: 'Menu',
   data(){
     return{
-
+      // 自己mock的数据
+      getMenuItems:{
+        1:{
+          'name':'黄焖鸡米饭',
+          'description':'加鸡腿',
+          'options':[{
+            'size':10,
+            'price':38
+          },{
+            'size':12,
+            'price':48
+          }]
+        },
+        2:{
+          'name':'兴国米粉鱼',
+          'description':'加鸡腿',
+          'options':[{
+            'size':10,
+            'price':38
+          },{
+            'size':12,
+            'price':48
+          }]
+        },
+        3:{
+          'name':'小鸡炖蘑菇',
+          'description':'加鸡腿',
+          'options':[{
+            'size':10,
+            'price':38
+          },{
+            'size':12,
+            'price':48
+          }]
+        }
+      }
     }
   },
   components:{
