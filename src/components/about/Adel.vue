0a1f25fb76c9dab173daec584ed582e6966dd8b3
526697296 <526697296@qq.com>
2018-05-21 08:47:48 +0800

    构建adel.vue

--- src/components/about/Adel.vue
+++ src/components/about/Adel.vue
@@ -1,25 +1,28 @@
 <template>
-  <div id="adel">
-    adel
-    
+  <div class="con">
+      <div class="mt-2 card bg-light text-dark">
+        <div class="card-header">联系我们</div>
+        <div class="card-body">
+            <div class="card-text">526697296@qq.com</div>
+            <div class="card-text">526697296@qq.com</div>
+        </div>
+        <div class="card-footer">
+            <a href="#" target="_blank" class="card-link">点我送惊喜~~</a>
+        </div>
+    </div>
   </div>
 </template>
-
 <script>
-
 export default {
-  name: 'Adel',
-  data(){
-    return{
-
-    }
+  name: "Con",
+  data() {
+    return {};
   },
-  components:{
-    
-  }
-}
+  components: {}
+};
 </script>
-
 <style scoped>
-
+    .card-footer a{
+        color:#999;
+    }
 </style>
