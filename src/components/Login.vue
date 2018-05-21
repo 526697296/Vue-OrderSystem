65fe359ccc0583280ab74605231a9214eada5d78
526697296 <526697296@qq.com>
2018-05-21 16:39:43 +0800

    完善登入，注册页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="login">
-    login
-    
+    <div class="row">
+      <div class="col-md-12 col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <img src="../../src/assets/login.jpg" class="mx-auto d-block">
+            <form @submit.prevent="onsubmit">
+              <div class="form-group">
+                <label for="email">账号</label>
+                <input type="email" class="form-control" v-model="email">
+              </div>
+              <div class="form-group">
+                <label for="password">密码</label>
+                <input type="password" class="form-control" v-model="password">
+              </div>
+              <button type="submit" class="btn btn-block btn-success">欢迎登入</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,15 +29,26 @@ export default {
   name: 'Login',
   data(){
     return{
-
+      email:'',
+      password:'',
     }
   },
   components:{
     
+  },
+  methods:{
+    onsubmit(){
+
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .card-body img{
+    height:36vh;
+    width: 100%;
+    border-radius: 6px;
+    opacity: 0.5;
+  }
 </style>
--- src/components/Reg.vue
+++ src/components/Reg.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="reg">
-    reg
-    
+    <div class="row">
+      <div class="col-md-12 col-lg-12">
+        <div class="card">
+          <div class="card-body">
+            <img src="../../src/assets/reg.jpg" class="mx-auto d-block">
+            <form @submit.prevent="onsubmit">
+              <div class="form-group">
+                <label for="email">邮箱</label>
+                <input type="email" class="form-control" v-model="email">
+              </div>
+              <div class="form-group">
+                <label for="password">密码</label>
+                <input type="password" class="form-control" v-model="password">
+              </div>
+              <div class="form-group">
+                <label for="confirm-password">确认密码</label>
+                <input type="password" class="form-control" v-model="confirmPassword">
+              </div>
+              <button type="submit" class="btn btn-block btn-success">注册账号</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,15 +33,27 @@ export default {
   name: 'Reg',
   data(){
     return{
-
+      email:'',
+      password:'',
+      confirmPassword:''
     }
   },
   components:{
     
+  },
+  methods:{
+    onsubmit(){
+
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .card-body img{
+    height:36vh;
+    width: 100%;
+    border-radius: 6px;
+    opacity: 0.2;
+  }
 </style>
